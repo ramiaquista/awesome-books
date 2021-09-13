9f7ed5b4cc763a36a33b48d65ded8cd0b4f65a87
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-09-13 17:43:24 -0300

    Add input form validators

---
+++ README.md
@@ -0,0 +1,67 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Personal Portfolio - Ramiro Aquistapace
+
+> In this project, We have set up a Library dynamically website.
+
+![screenshot](./readme-img.png)
+
+
+## Built With
+
+- HTML - CSS - JavaScript
+- Git - Github - GitFlow
+
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+- Visual Studio Code or Sublime Text
+- Git
+
+
+### Setup
+
+Go to the top of the page, press te green button that says "Code", and copy the link. Then you have to go to your console and type " git clone 'repository-url.com' ".
+
+That's all, you are ready to go!
+
+### Install
+
+No extra dependencies needed to use the code!
+
+### Usage
+
+Add Live Server VSCode extension to deploy a local host server in order to start using it!
+
+### Live Demo Server
+
+<!-- - [My Portfolio](https://ramiaquista.github.io/ramiaq-portfolio/) -->
+
+
+
+
+## Authors
+
+👤 **Author : Ramiro Aquistapace**
+
+- GitHub: [@ramiaquista](https://github.com/ramiaquista)
+- Twitter: [@ramiaq1](https://twitter.com/ramiaq1)
+- LinkedIn: [RamiroAquistapace](https://www.linkedin.com/in/ramiro-aquistapace-32b61b204/)
+
+👤 **Author :  Jose Abel Ramirez**
+
+- GitHub: [Jose Abel Ramirez](https://github.com/jose-Abel)
+- Linkedin: [Jose Abel Ramirez Frontany](https://www.linkedin.com/in/jose-abel-ramirez-frontany-7674a842/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- index.js
+++ index.js
@@ -1,4 +1,55 @@
+/* eslint-disable no-use-before-define */
 let library = [];
+
+function setLocalStorage() {
+  localStorage.setItem('library', JSON.stringify(library));
+}
+
+function getLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('library'));
+
+  if (!data) return;
+
+  library = data;
+}
+
+const displayBook = (library) => {
+  const booklist = document.querySelector('.book-list');
+
+  if (booklist.hasChildNodes()) {
+    const nodesArray = document.querySelectorAll('li');
+    nodesArray.forEach((node) => {
+      booklist.removeChild(node);
+    });
+  }
+  if (library.length > 0) {
+    library.forEach((book) => {
+      const li = document.createElement('li');
+      const p1 = document.createElement('p');
+      const p2 = document.createElement('p');
+      const removeBtn = document.createElement('button');
+      const hr = document.createElement('hr');
+
+      p1.innerHTML = book.title;
+      p2.innerHTML = book.author;
+      removeBtn.innerHTML = 'Remove';
+      removeBtn.classList.add('remove-button');
+      li.appendChild(p1);
+      li.appendChild(p2);
+      li.appendChild(removeBtn);
+      li.appendChild(hr);
+      booklist.appendChild(li);
+    });
+  }
+
+  const allDeleteButtons = document.querySelectorAll('.remove-button');
+
+  if (allDeleteButtons.length > 0) {
+    allDeleteButtons.forEach((button) => {
+      button.addEventListener('click', deleteThisBook.bind(this));
+    });
+  }
+};
 
 const Book = (title, author) => {
   library.push({ title, author });
@@ -19,64 +70,23 @@ const deleteThisBook = (button) => {
   const titleToDelete = button.target.parentNode.firstElementChild.innerHTML;
   removeBook(titleToDelete);
   displayBook(library);
-}
-
-function getLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('library'));
-
-  if (!data) return;
-
-  library = data;
-}
-
-function setLocalStorage() {
-  localStorage.setItem('library', JSON.stringify(library));
-}
-
-const displayBook = (library) => {
-  const booklist = document.querySelector('.book-list');
-
-  if (booklist.hasChildNodes()) {
-    const nodesArray = document.querySelectorAll('li');
-    nodesArray.forEach((node) => {
-        booklist.removeChild(node);
-    });
-  }
-  if (library.length > 0) {
-    library.forEach((book) => {
-      let li = document.createElement('li');
-      let p1 = document.createElement("p");
-      let p2 = document.createElement("p");
-			let removeBtn = document.createElement("button");
-			let hr = document.createElement("hr");
-
-			p1.innerHTML = book.title;
-			p2.innerHTML = book.author;
-			removeBtn.innerHTML = "Remove";
-			removeBtn.classList.add("remove-button");
-			li.appendChild(p1);
-			li.appendChild(p2);
-			li.appendChild(removeBtn);
-			li.appendChild(hr);
-			booklist.appendChild(li);
-    });
-  }
-
-	const allDeleteButtons = document.querySelectorAll(".remove-button");
-
-	if(allDeleteButtons.length > 0) {
-    allDeleteButtons.forEach((button) => {
-      button.addEventListener("click", deleteThisBook.bind(this));
-    });
-	}
 };
 
 const addBook = () => {
-  let titleBook = document.getElementById('titleBook').value;
-  let authorBook = document.getElementById('authorBook').value;
+  const titleBook = document.getElementById('titleBook').value;
+  const authorBook = document.getElementById('authorBook').value;
+  if (titleBook === '' || authorBook === '') {
+    return;
+  }
+  if (library.length > 0) {
+    library.forEach((book) => {
+      if (titleBook === book.title) {
+        throw new Error('Book already has been already added.');
+      }
+    });
+  }
   Book(titleBook, authorBook);
 };
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const addBookBtn = document.querySelector('.addBook');
