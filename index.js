fc93e9438b1a9599930dff734f721a5afb859ff9
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-09-13 14:44:33 -0300

    Add book functional btn

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 let library = [];
 
 const Book = (title, author) => {
-    library.push({title, author});
+  library.push({ title, author });
 };
 
 const removeBook = (title) => {
-    library = library.filter((book) => book.title !== title);
-}
+  library = library.filter((book) => book.title !== title);
+};
+
+const booklist = document.querySelector('.book-list');
+const addBookBtn = document.querySelector('.addBook');
+
+const displayBook = (library) => {
+  if (booklist.hasChildNodes()) {
+    const nodeArray = document.querySelectorAll('li');
+    nodeArray.forEach((node) => {
+        booklist.removeChild(node);
+    })
+  }
+  if (library.length > 0) {
+    library.forEach((book) => {
+      let li = document.createElement('li');
+      li.innerHTML = book.title;
+      booklist.appendChild(li);
+    });
+  }
+};
+
+const addBook = () => {
+  let titleBook = document.getElementById('titleBook').value;
+  let authorBook = document.getElementById('authorBook').value;
+  Book(titleBook, authorBook);
+  displayBook(library);
+};
+
+addBookBtn.addEventListener('click', () => {
+  addBook();
+});
