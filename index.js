c16807fe741668a1546e0a1fb13dd58f0af8feca
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-16 11:39:21 -0400

    Add luxon cdn library. Create display time method and render the time

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-use-before-define */
+// import {DateTime } from "luxon";
+
 class AwesomeBook {
+  DateTime = luxon.DateTime;
+  dt = this.DateTime.now();
+
   library = [];
 
   setLocalStorage() {
@@ -14,8 +19,27 @@ class AwesomeBook {
     this.library = data;
   }
 
+  displayTime() {
+    const span = document.createElement('span');
+    const mainTitle = document.querySelector('.main-title');
+
+    let dateToDisplay = this.dt.toLocaleString(this.DateTime.DATETIME_FULL_WITH_SECONDS);
+    dateToDisplay = dateToDisplay.replace("AST", "")
+    span.innerHTML = dateToDisplay
+
+    mainTitle.style.position = "relative";
+    span.style.position = "absolute";
+    span.style.right = '1rem';
+    span.style.top = '4rem';
+    mainTitle.style.marginTop = "3rem"; 
+
+    mainTitle.insertAdjacentElement("afterend", span);
+  }
+
   displayBooks(library) {
     const booklist = document.querySelector('.book-list');
+    
+    this.displayTime();
 
     if (booklist.hasChildNodes()) {
       const nodesArray = document.querySelectorAll('li');
@@ -119,6 +143,9 @@ class AwesomeBook {
       });
     }
     this.Book(titleBook, authorBook);
+    document.getElementById('titleBook').value = '';
+    document.getElementById('authorBook').value = '';
+
     errorP = document.querySelector('.error-p');
     if (errorP !== null) {
       errorP.remove();
@@ -142,3 +169,4 @@ class AwesomeBook {
 const awesomeBook = new AwesomeBook();
 
 awesomeBook.main();
+
